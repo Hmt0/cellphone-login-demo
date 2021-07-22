574d301d130f59a6787dd1f985f46811004233f6
Hmt0 <luna630h@163.com>
2021-07-22 23:24:13 +0800

    组件排布

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="container">
+        <p className="title">请登录！</p>
+        <div className="formRow">
+          <label htmlFor="cellphone">请输入电话号码：</label>
+          <input type="text" id="cellphone" required></input>
+        </div>
+
+        <div className="formRow">
+          <label htmlFor="verifyCode">请输入验证码：</label>
+          <input type="text" id="verifyCode" required></input>
+          <button className="sendButton" disabled>发送验证码</button>
+        </div>
+        <button>立即登录</button>
+      </form>
     </div>
   );
 }
